79b7287748568f6708c540c2a8deb71446dcc561
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-03-24 17:28:38 +0200

    spring demo project. udemy course. Spring Bean

---
+++ src/com/luv2code/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.luv2code.springdemo;
+
+public interface Coach {
+
+    //only what is available without description of the method
+    public String getDailyWorkout();
+}
---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+    //automatically created implementation
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}
