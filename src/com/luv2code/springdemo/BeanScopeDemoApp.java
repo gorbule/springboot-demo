c8ff185b7edecb9fabef16f9e95eabfd95237b71
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-03-25 13:57:41 +0200

    spring boot demo project. bean scopes. checking if they are the same

--- src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -12,7 +12,20 @@ public class BeanScopeDemoApp {
         //retrieve bean(s) from String Container
         Coach theCoach = context.getBean("myCoach", Coach.class);
 
-        Coach aphaCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        //check if they are the same
+        boolean result = (theCoach == alphaCoach);
+        //print put the result
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+
+        //close the context
+        context.close();
+
 
     }
 }
